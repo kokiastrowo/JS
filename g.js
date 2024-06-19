514cd7f80e0205207b33350e2e372e9dadce412e
Swarali kulkarni <123886703+kokiastrowo@users.noreply.github.com>
2024-06-19 02:37:58 +0000

    BASIC DONE

---
+++ g.js
@@ -0,0 +1,12 @@
+const accountID=144553 // CANNOT CHANGE
+let accountEmail="svk@gmail.com"
+var accountPassword="123"
+accountCity="Pune"
+let accountState;// undefined if no value assigned DOESNT GIVE ERROR 
+//accountID=233 // cannot change 
+accountEmail="hfbdv"
+accountPassword="566"
+accountCity=23 
+console.table([accountEmail,accountPassword,accountCity]) // all these changes 
+console.log(accountID);
+
