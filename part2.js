0a83f5894f19af2f545070a3b1f199df3c28c8bb
Swarali kulkarni <123886703+kokiastrowo@users.noreply.github.com>
2024-06-19 02:55:24 +0000

    DATATYPES

---
+++ part2.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js as a newer version 
+// dont use alert 
+// ecma and mdn use for datatypes 
+//NULL =>IS A STANDALONE VALUE
+//UNDEFINED =>NOT ASSIGNED 
+//SYMBOL => FOR UNIQUENESS 
+//OBJECT
+console.log(typeof"swarali") //String
+console.log(typeof null)// object => few people call it limitations of language 
+console.log(typeof undefined)//undefined
+
