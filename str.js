b9cbff6d5ff0bcdbc4a079d762c973edc7dd40aa
Swarali kulkarni <123886703+kokiastrowo@users.noreply.github.com>
2024-06-20 15:12:49 +0000

    String in js

---
+++ str.js
@@ -0,0 +1,14 @@
+//new syntax
+const name ="hfgvd"
+const age =20
+console.log(`Hello my name is ${name} and my age is ${age}`)
+let ns= new String('Swarali')
+//gives the key val pairs 0:S ,1:w
+console.log(ns[3])
+console.log(ns.toUpperCase())
+// ns="jfrhjnf"// ns is changed as it is a primitive dataype and it gives a copy ...
+//charAt,indexOf,trim,replace('%20','-')=> str,includes=> t/f , split('-') based on the separator '-'
+console.log(ns)
+const news=ns.substring(0,4) //.start to length => whole string
+const ne=ns.slice(0,4)
+console.log(ne)
