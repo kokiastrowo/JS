9711426ae9c930fcb7c0d57c1281d7324c6b76e9
Swarali kulkarni <123886703+kokiastrowo@users.noreply.github.com>
2024-06-20 14:11:34 +0000

    OPERATIONS IN JS

---
+++ op.js
@@ -0,0 +1,19 @@
+//operations 
+//we can 2 strings easily
+//ALWAYS KEEP IN MIND  
+console.log("1"+2)//12
+console.log(1+"2")//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32 String first then all are strings 
+console.log(-true); //-1 +true => +1
+console.log(true);
+let c=100;
+console.log(c++); 100
+console.log(++c); 102
+//COMPARISIONS
+console.log("2">1)//true // diff datatypes => cannot be compared
+console.log("02">1)//true
+console.log(null >=0 )//true
+console.log(null >0 )//false
+console.log(null ==0 )//false
+console.log("2"===2)// strict checking //false
